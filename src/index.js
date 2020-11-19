1415d79cfc3a716b3756ab14688a7cf9d56f6784
nic-ball <nicholasball147@gmail.com>
2020-11-19 13:28:48 +0000

    Added server for connecting to github api

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const { graphqlHTTP } = require("express-graphql");
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolvers = require("./graphql/resolvers");
+
+const app = express();
+
+app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers,
+    graphiql: true
+  })
+);
+
+
+app.listen(3000, () => console.log("Server is running on localhost:3000"));
